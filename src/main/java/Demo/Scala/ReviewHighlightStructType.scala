5d1d8e6ad57ab1dc045e478dfc3f957b78563676
pankaj upadhayay <oankaj.upadhayay@bazaarvoice.com>
2021-04-13 19:20:17 +0530

    update structtype

--- src/main/java/Demo/Scala/ReviewHighlightStructType.scala
+++ src/main/java/Demo/Scala/ReviewHighlightStructType.scala
@@ -8,59 +8,57 @@ object ReviewHighlightStructType extends App {
   val spark = SparkSession.builder().appName("SparkByExamples.com")
     .master("local[1]")
     .getOrCreate()
-
+  //This is sample data need to populate after retrieval from spark SQL
   val structureData = Seq(
-    Row(Row(Row(Row(234, 345, Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle"))))),
-    Row(Row(Row(Row(134, 145, Row("rating1", "about1", "reviewText1", "author1", "reviewText1", "author1", "snippetId1", "summary1", "submissionTime1", "reviewTitle1"))))),
-    Row(Row(Row(Row(334, 445, Row("rating2", "about2", "reviewText2", "author2", "reviewText2", "author2", "snippetId2", "summary2", "submissionTime2", "reviewTitle2"))))))
+    Row(Row(Map("fit" -> Row(234, 345, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")
+      , Row("rating12", "about12", "reviewText12", "author34", "reviewText454", "author56", "snippetId56", "summary56", "submissionTime67", "reviewTitle65"))),
+      "price" -> Row(34, 35, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")
+        , Row("rating12", "about12", "reviewText124", "author341", "reviewText45", "author568", "snippetId568", "summary568", "submissionTime678", "reviewTitle658"))),
+      "satisfaction" -> Row(24, 35, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")
+        , Row("rating12", "about12", "reviewText125", "author342", "reviewText456", "author567", "snippetId567", "summary567", "submissionTime677", "reviewTitle657"))),
+      "comford" -> Row(23, 34, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")
+        , Row("rating12", "about12", "reviewText126", "author343", "reviewText457", "author564", "snippetId56", "summary564", "submissionTime67", "reviewTitle654")))
+    ), Map("price" -> Row(234, 345, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")))))))
 
+  val positive_feature = "Best"
+  val negative_feature = "Worse"
 
-  val structureData2 = Seq(
-    Row(Row(Row(Row(234, 345, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")
-                                  ,Row("rating12", "about12", "reviewText12", "author34", "reviewText45", "author56", "snippetId56", "summary56", "submissionTime67", "reviewTitle65"))))
-      , Row(Row(234, 345, List(Row("rating", "about", "reviewText", "author", "reviewText", "author", "snippetId", "summary", "submissionTime", "reviewTitle")))))))
-
-
-
-  val positive_feature = "price"
-  val negative_feature = "color"
+  //This is Schema for Review Highlight display API
   val structureSchema = new StructType()
     .add("subjects", new StructType()
-      .add("positive", new StructType()
-        .add(positive_feature, new StructType()
-          .add("presenceCount", IntegerType)
-          .add("mentionsCount", IntegerType)
-          .add("bestExamples", ArrayType(new StructType()
-            .add("rating", StringType)
-            .add("about", StringType)
-            .add("reviewText", StringType)
-            .add("author", StringType)
-            .add("snippetId", StringType)
-            .add("summary", StringType)
-            .add("submissionTime", StringType)
-            .add("reviewTitle", StringType))
-          )
+      .add(positive_feature, MapType(StringType, new StructType()
+        .add("presenceCount", IntegerType)
+        .add("mentionsCount", IntegerType)
+        .add("bestExamples", ArrayType(new StructType()
+          .add("rating", StringType)
+          .add("about", StringType)
+          .add("reviewText", StringType)
+          .add("author", StringType)
+          .add("snippetId", StringType)
+          .add("summary", StringType)
+          .add("submissionTime", StringType)
+          .add("reviewTitle", StringType))
         )
       )
-      .add("negative", new StructType()
-        .add(negative_feature, new StructType()
-          .add("presenceCount", IntegerType)
-          .add("mentionsCount", IntegerType)
-          .add("bestExamples", ArrayType(new StructType()
-            .add("rating", StringType)
-            .add("about", StringType)
-            .add("reviewText", StringType)
-            .add("author", StringType)
-            .add("snippetId", StringType)
-            .add("summary", StringType)
-            .add("submissionTime", StringType)
-            .add("reviewTitle", StringType))
-          )
+      )
+      .add(negative_feature, MapType(StringType, new StructType()
+        .add("presenceCount", IntegerType)
+        .add("mentionsCount", IntegerType)
+        .add("bestExamples", ArrayType(new StructType()
+          .add("rating", StringType)
+          .add("about", StringType)
+          .add("reviewText", StringType)
+          .add("author", StringType)
+          .add("snippetId", StringType)
+          .add("summary", StringType)
+          .add("submissionTime", StringType)
+          .add("reviewTitle", StringType))
         )
+      )
       )
     )
 
-  val arrayDF = spark.createDataFrame(spark.sparkContext.parallelize(structureData2), structureSchema)
+  val arrayDF = spark.createDataFrame(spark.sparkContext.parallelize(structureData), structureSchema)
   arrayDF.printSchema()
   arrayDF.show()
 
@@ -69,6 +67,6 @@ object ReviewHighlightStructType extends App {
 
   //printing json
   arrayDF.write.mode("overwrite")
-    .json("src/test/resources/output/StructTypeBasic11.json")
+    .json("src/test/resources/output/ReviewHighlight.json")
 
 }
